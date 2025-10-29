d9c8a291deb67acbc21d7389d58d7038c232c7a5
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2025-10-29 20:32:11 +0100

    Update provision script for Ubuntu 24

--- ubuntu-24.04/scripts/provision.sh
+++ ubuntu-24.04/scripts/provision.sh
@@ -11,6 +11,18 @@ error() {
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
+passwd -d root
+echo 'root:vagrant' | chpasswd -m
+passwd -d vagrant
+echo 'vagrant:vagrant' | chpasswd -m
+
+# SSH config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication/PasswordAuthentication/' /etc/ssh/sshd_config
+sed -i 's/KbdInteractiveAuthentication no/KbdInteractiveAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/#KbdInteractiveAuthentication/KbdInteractiveAuthentication/' /etc/ssh/sshd_config
+systemctl restart ssh
+
 # Run clean/autoclean/purge/update first, this will work around problems with ghost packages, and/or
 # conflicting data in the repo index cache. After the cleanup is complete, we can proceed with the 
 # update/upgrade/install commands below.
@@ -19,8 +31,12 @@ apt-get --assume-yes autoclean ; error
 apt-get --assume-yes update ; error
 
 # Upgrade the installed packages.
-retry apt-get --assume-yes -o Dpkg::Options::="--force-confnew" upgrade ; error
+apt-get --assume-yes -o Dpkg::Options::="--force-confnew" upgrade ; error
 apt-get --assume-yes -o Dpkg::Options::="--force-confnew" upgrade ; error
 
 # Needed to retrieve source code, and other misc system tools.
-apt-get --assume-yes install vim vim-nox git wget curl rsync gnupg mlocate sudo sysstat lsof pciutils usbutils lsb-release psmisc ; error
+SOFTWARE="vim vim-nox nano openssh-server git wget curl rsync sshpass unzip dnsutils dos2unix whois gnupg sudo sysstat lsof pciutils usbutils lsb-release psmisc"
+echo "==> Installing software packages..."
+apt-get --assume-yes install $SOFTWARE ; error
+echo "==> done"
+
