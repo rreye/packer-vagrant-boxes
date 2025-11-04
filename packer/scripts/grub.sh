e0dfca4d9fd5c75a97a706778d29078ecd58c853
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2025-11-04 10:49:36 +0100

    Fix broken GRUB for RHEL

--- packer/scripts/grub.sh
+++ packer/scripts/grub.sh
@@ -1,18 +1,36 @@
 #!/bin/sh -eux
 
-echo "==> Configuring Grub..."
+echo "==> Configuring GRUB..."
 
 NEW_TIMEOUT=5  # segundos
 DISTRO_ID=$(grep -E '^ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
+
+set_grub_timeout() {
+  FILE="$1"
+  sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=${NEW_TIMEOUT}/" "$FILE" \
+    || echo "GRUB_TIMEOUT=${NEW_TIMEOUT}" >> "$FILE"
+}
+
+generate_grub_cfg_rhel() {
+  if [ -d /sys/firmware/efi ]; then
+    # EFI
+    EFI_VENDOR=$(ls /boot/efi/EFI | head -n1)
+    grub2-mkconfig -o /boot/efi/EFI/${EFI_VENDOR}/grub.cfg
+  else
+    # BIOS
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+  fi
+}
 
 echo "==> Detecting distro: $DISTRO_ID"
 
 case "$DISTRO_ID" in
   ubuntu|debian)
     echo "-> Ubuntu/Debian"
+    
     if [ -f /etc/default/grub ]; then
-    	sudo sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=${NEW_TIMEOUT}/" /etc/default/grub
-    	sudo update-grub
+    	set_grub_timeout /etc/default/grub
+    	update-grub
     else
     	echo "   No /etc/default/grub found. Skipping."
     fi
@@ -20,9 +38,10 @@ case "$DISTRO_ID" in
 
   rocky|rhel|centos|almalinux|fedora|opensuse*|suse)
     echo "-> RHEL/SUSE family"
+    
     if [ -f /etc/default/grub ]; then
-    	sudo sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=${NEW_TIMEOUT}/" /etc/default/grub
-    	sudo grub2-mkconfig -o /etc/default/grub
+    	set_grub_timeout /etc/default/grub
+	generate_grub_cfg_rhel
     else
     	echo "   No /etc/default/grub found. Skipping."
     fi
@@ -34,11 +53,11 @@ case "$DISTRO_ID" in
       	echo "   (syslinux)"
       	# syslinux usa décimas de segundo
       	SYSL_TIMEOUT=$((NEW_TIMEOUT * 10))
-      	sudo sed -i "s/^TIMEOUT.*/TIMEOUT ${SYSL_TIMEOUT}/" /boot/syslinux/syslinux.cfg
+      	sed -i "s/^TIMEOUT.*/TIMEOUT ${SYSL_TIMEOUT}/" /boot/syslinux/syslinux.cfg
     elif [ -f /etc/default/grub ]; then
       	echo "   (GRUB)"
-      	sudo sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=${NEW_TIMEOUT}/" /etc/default/grub
-      	sudo grub-mkconfig -o /boot/grub/grub.cfg
+      	set_grub_timeout /etc/default/grub
+      	grub-mkconfig -o /boot/grub/grub.cfg
     else
     	echo "   (efibootmgr/aboot or unknown bootloader)"
       	echo "   No GRUB/syslinux config found. This is normal for fast-boot EFI."
@@ -52,6 +71,6 @@ case "$DISTRO_ID" in
     ;;
 esac
 
-echo "Timeout set to ${NEW_TIMEOUT}s."
+echo "GRUB timeout set to ${NEW_TIMEOUT}s."
 
-echo "==> Grub configuration complete."
+echo "==> GRUB configuration complete."
